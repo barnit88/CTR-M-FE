1e584325c1c693e398965bde3d3382879a8886dd
Nischal Shrestha <nischalshrestha81@gmail.com>
2022-07-11 14:38:44 -0700

    new chnages

--- src/app/services/api-service/EmployeeService/employee.service.ts
+++ src/app/services/api-service/EmployeeService/employee.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { AppSettings } from 'src/app/app.setting';
+import { Client } from 'src/app/entity/models/Client/client';
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  startedEditing = new Subject<number>();
+  listChanged = new Subject<Client>();
+  
+  constructor(private http:HttpClient) { }
+  add(entity:Client):Observable<Client>{
+return this.http.post<Client>(AppSettings.API_ROOT+'Employee',entity);
+  }
 
-  constructor() { }
 }
