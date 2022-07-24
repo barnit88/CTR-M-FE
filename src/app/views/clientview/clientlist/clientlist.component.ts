415a3c66d9f0152c078fab1ceb0d33634fbc5015
Nischal Shrestha <nischalshrestha81@gmail.com>
2022-07-24 12:10:27 -0700

    changes made on client component

--- src/app/views/clientview/clientlist/clientlist.component.ts
+++ src/app/views/clientview/clientlist/clientlist.component.ts
@@ -6,89 +6,64 @@ import { ClientcreateComponent } from '../clientcreate/clientcreate.component';
 import { ClientService } from './../../../services/api-service/ClientService/client.service';
 import { Client } from 'src/app/entity/models/Client/client';
 import { filter } from 'rxjs';
-import { IComponent } from '../../IComponent';
 import { Buttons, ClientTitle } from 'src/app/entity/Enum/Enums';
-
+import { ThisReceiver } from '@angular/compiler';
 @Component({
   selector: "app-clientlist",
   templateUrl: "./clientlist.component.html",
   styleUrls: ["./clientlist.component.css"],
 })
-export class ClientlistComponent implements OnInit,IComponent<Client> {
-    data: Client[];
-    title: string =ClientTitle.List;
+export class ClientlistComponent implements OnInit {
+    data: Client[]=[];
+    title: string = 'Client Details';
   constructor(private genericDetailPopUpService: GenericDetailPopUpService,
      private genericModalPopUpService: GenericModalPopUpService,
      private clientService:ClientService,
      public buttonTitle :Buttons
      ) {  }
-
   ngOnInit(): void {
-    // this.onGetClientList();
+    this.onGetClientList();
+  }
+  
+  onGetClientList(): any {
+    this.clientService.getAll().subscribe((response)=> this.data = response,
+      (error:any)=> console.log(error),
+      ()=> console.log("Done with fetching  Client  list")); 
+    console.log(this.data);
   }
 
-  // onGetClientList(): any{
-  //     (response)=> this.data = response,
-  //     (error:any)=> console.log(error),
-  //     ()=> console.log("Done with fetching  Client  list") 
-  //     () => console.log('Done with fetching  Client  list')
-  //   console.log(this.data)
-  //   console.log(this.data);
-  // }
+  onGetSingleClient(id:number): any{
+      this.clientService.getById(id).subscribe((response)=> console.log(response),
+      (error:any)=> console.log(error),
+      ()=> console.log('done with geeting single  Client  by id ')
+    )
+  }
 
-  // onGetSingleClient(id:number): any{
-  //     (response)=> console.log(response),
-  //     (error:any)=> console.log(error),
-  //     ()=> console.log('done with geeting single  Client  by id '));
-  //   );
-  // }
+  deleteClientById(id:number): any {  
+    var ans = confirm("Do you want to delete  Client  with Id: " + id);  
+    if (ans) {  
+        this.clientService.delete(id).subscribe((data: any) => { 
+          console.log('Sucess on deleting  Client ')
+        }, (error: any) => console.error(error))  
+    } 
+    }
 
-  // deleteClientById(id:number): any {  
-  //   var ans = confirm("Do you want to delete  Client  with Id: " + id);  
-  //   if (ans) {  
-  //       this.clientService.delete(id).subscribe((data: any) => { 
-  //         console.log('Sucess on deleting  Client ')
-  //       }, (error: any) => console.error(error))  
-  //   } 
+    OpenModalPopUp() {
+      this.genericModalPopUpService.openModalPopUpService<Client>(ClientcreateComponent, 
+        new Client(),
+        ClientTitle.Create,
+      );
+    }
    
-
-    OpenCreate():void{
-      this.genericModalPopUpService.openModalPopUpService<Client>(ClientcreateComponent, 
-        new Client(),
-        ClientTitle.Create        
-      );
+    OpenDetailPopUp(id: number) {
+      this.genericModalPopUpService.openModalPopUpService<Client>(ClientdetailComponent,
+        this.data.find(x => x.Id == id),
+        ClientTitle.Detail);
     }
     
-    OpenEdit(id:number):void{
-      this.genericModalPopUpService.openModalPopUpService<Client>(ClientcreateComponent, 
-        new Client(),
-        ClientTitle.Edit        
-      );
+    OpenEditPopUp(id: number) {
+      this.genericModalPopUpService.openModalPopUpService<Client>(ClientcreateComponent,
+        this.data.find(x => x.Id == id),
+        ClientTitle.Edit);
     }
-    
-    OpenDetail(id:number):void{
-      this.genericModalPopUpService.openModalPopUpService<Client>(ClientdetailComponent, 
-        new Client(),
-        ClientTitle.Detail
-      );
-    }
-
-    OpenDelete(id:number):void{
-
-    }
-
-  //   OpenDetailPopUp(id: number) {
-  //     this.genericModalPopUpService.openModalPopUpService<Client>(ClientdetailComponent,
-  //       this.data.find(x => x.Id == id),
-  //       "Employee Payment Details",
-  //       id)
-
-  //   }
-  
-  //   OpenEditPopUp(id: number) {
-  //     this.genericModalPopUpService.openModalPopUpService<Client>(ClientcreateComponent,
-  //       this.data.find(x => x.Id == id),
-  //       "Employee Payment Edit",
-  //       id);
-  //   }
-}
+  }
